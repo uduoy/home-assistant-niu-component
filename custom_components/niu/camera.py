dc1c5bd9ce5aeb488489d96431f0455bbed9ae1f
uduoy <16486011+uduoy@user.noreply.gitee.com>
2026-05-21 21:08:19 +0800

    fix: move framerate into advanced section (new GenericCamera format)

--- custom_components/niu/camera.py
+++ custom_components/niu/camera.py
@@ -48,10 +48,10 @@ async def async_setup_entry(hass, entry, async_add_entities) -> None:
         "password": None,
         "limit_refetch_to_url_change": False,
         "content_type": "image/jpeg",
-        "framerate": 2,
         "verify_ssl": False,
         "advanced": {
             "limit_refetch_to_url_change": False,
+            "framerate": 2,
         },
     }
     async_add_entities([LastTrackCamera(hass, api, coordinator, device_config, camera_name, camera_name)])
