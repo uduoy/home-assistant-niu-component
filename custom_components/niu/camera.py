1b6ac084125c0cecf50ee124b82574b6a07fc739
uduoy <16486011+uduoy@user.noreply.gitee.com>
2026-05-21 21:40:40 +0800

    fix: move verify_ssl into advanced section (GenericCamera format)

--- custom_components/niu/camera.py
+++ custom_components/niu/camera.py
@@ -48,10 +48,10 @@ async def async_setup_entry(hass, entry, async_add_entities) -> None:
         "password": None,
         "limit_refetch_to_url_change": False,
         "content_type": "image/jpeg",
-        "verify_ssl": False,
         "advanced": {
             "limit_refetch_to_url_change": False,
             "framerate": 2,
+            "verify_ssl": False,
         },
     }
     async_add_entities([LastTrackCamera(hass, api, coordinator, device_config, camera_name, camera_name)])
